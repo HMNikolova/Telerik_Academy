dc73ee529601bf0c9c3102f8e947fa42489b7410
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 09:35:26 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/02. BonusScore/BonusScore.cs
+++ CSharpPartOne/5. ConditionalStatements/02. BonusScore/BonusScore.cs
@@ -11,7 +11,31 @@ using System;
     {
         static void Main()
         {
+        Console.WriteLine("Give score from 1 to 9");
+        int score = int.Parse(Console.ReadLine());
+		switch (score)
+        {
+            case 1:
+            case 2:
+            case 3:
+                Console.WriteLine("Result: {0}", score * 10);
+                break;
 
+            case 4:
+            case 5:
+            case 6:
+                Console.WriteLine("Result: {0}", score * 100);
+                break;
+
+            case 7:
+            case 8:
+            case 9:
+                Console.WriteLine("Result: {0}", score * 1000);
+                break;
+
+            default: Console.WriteLine("Invalid score");
+                break;
+        }
         }
     }
 
