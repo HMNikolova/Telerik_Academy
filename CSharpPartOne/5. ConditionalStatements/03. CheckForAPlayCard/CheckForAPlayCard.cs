6819fe345487e05ba453b88e69bc51265e672c3e
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 09:54:23 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/03. CheckForAPlayCard/CheckForAPlayCard.cs
+++ CSharpPartOne/5. ConditionalStatements/03. CheckForAPlayCard/CheckForAPlayCard.cs
@@ -7,7 +7,28 @@ using System;
     {
         static void Main()
         {
-
+        Console.Write("Enter a play card: ");
+        string card = Console.ReadLine();
+		switch (card)
+		    {
+    		    case "2":
+    			case "3":
+    			case "4":
+    			case "5":
+    			case "6":
+    			case "7":
+    			case "8":
+    			case "9":
+    			case "10":
+    			case "J":
+                case "Q":
+                case "K":
+                case "A":
+    				Console.WriteLine("Yes"); 
+    			break;
+    			default:
+    				Console.WriteLine("No"); 
+    			break;
+            }
         }
     }
-
