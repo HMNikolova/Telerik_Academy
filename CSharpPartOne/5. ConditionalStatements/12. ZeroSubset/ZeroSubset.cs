acc0466566b2751fbb8a853488507cc47388abff
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 14:19:03 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/12. ZeroSubset/ZeroSubset.cs
+++ CSharpPartOne/5. ConditionalStatements/12. ZeroSubset/ZeroSubset.cs
@@ -9,65 +9,48 @@ class ZeroSubset
 {
     static void Main()
     {
-        int[] inputNumbers = new int[5];
+        Console.Write("Enter five integer separated by space: ");
+        string input = Console.ReadLine();
 
-        for (int i = 0; i < 5; i++)
+        string[] splittedInput = input.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int[] numbers = new int[splittedInput.Length];
+
+        for (int i = 0; i < splittedInput.Length; i++)
         {
-            Console.WriteLine("Enter number {0}: ", i + 1);
-            inputNumbers[i] = int.Parse(Console.ReadLine());
+            numbers[i] = int.Parse(splittedInput[i]);
         }
-
-        int count = 0;
-        List<List<int>> finalOut = new List<List<int>>();
-
-        /*
-         * Since we have 5 integers, we will create something similar to a bit mask (but for an entire number, not a bit) to match each of the numbers. 
-         * That way we iterate through all of the possible combinations with ease. If the bit is 1, we add up to the sum and then check whether the sum equals 0.
-         */
 
         for (int i = 1; i <= 31; i++)
         {
-            List<int> outNumbers = new List<int>();
-            int currentSum = 0;
-            for (int j = 0; j < 5; j++)
+            List<int> subSetNumbers = new List<int>();
+            int sum = 0;
+
+            for (int j = 0; j < numbers.Length; j++)
             {
-                int mask = 1 << j;        
-                int nAndMask = i & mask;  
-                int bit = nAndMask >> j;  
+                int bit = (i >> j) & 1;
+
                 if (bit == 1)
                 {
-                    currentSum += inputNumbers[j];
-                    outNumbers.Add(inputNumbers[j]);
+                    sum += numbers[j];
+                    subSetNumbers.Add(numbers[j]);
                 }
             }
-            if (currentSum == 0)
+            if (sum == 0)
             {
-                count++;
-                // TODO: Compare the contents of the parent list members with the current instance of outNumbers to avoid duplicates in the result.
-                finalOut.Add(outNumbers);
-            }
-        }
-        if (count > 0)
-        {
-            foreach (var sublist in finalOut)
-            {
-                int itCount = 0;
-                foreach (var value in sublist)
+                for (int j = 0; j < subSetNumbers.Count; j++)
                 {
-                    Console.Write(value);
-                    if (itCount != sublist.Count - 1)
+                    if (j == subSetNumbers.Count - 1)
                     {
-                        Console.Write('+');
+                        Console.Write("{0}", subSetNumbers[j]);
                     }
-                    itCount++;
+                    else
+                    {
+                        Console.Write("{0} + ", subSetNumbers[j]);
+                    } 
                 }
-                Console.Write(" = 0");
-                Console.WriteLine();
+                Console.WriteLine(" = " + sum);
             }
-        }
-        else
-        {
-            Console.WriteLine("No zero subset.");
         }
     }
 }
