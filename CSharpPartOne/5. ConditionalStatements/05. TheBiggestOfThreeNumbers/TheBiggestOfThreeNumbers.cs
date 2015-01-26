398c96f516f9ad96ea02e41214617c838f5555fd
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 10:22:30 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/05. TheBiggestOfThreeNumbers/TheBiggestOfThreeNumbers.cs
+++ CSharpPartOne/5. ConditionalStatements/05. TheBiggestOfThreeNumbers/TheBiggestOfThreeNumbers.cs
@@ -8,11 +8,11 @@ using System;
         static void Main()
         {
             Console.Write("Enter the first number: ");
-            int a = int.Parse(Console.ReadLine());
+            double a = double.Parse(Console.ReadLine());
             Console.Write("Enter the second number: ");
-            int b = int.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
             Console.Write("Enter the third number: ");
-            int c = int.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
     
             if (a > b && a > c)
             {
