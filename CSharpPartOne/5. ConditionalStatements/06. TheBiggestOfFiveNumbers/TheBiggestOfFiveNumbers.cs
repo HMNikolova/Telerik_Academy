2ca86599dbb3b40cbb417cd4e49a3ffa598e3de2
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 10:38:29 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/06. TheBiggestOfFiveNumbers/TheBiggestOfFiveNumbers.cs
+++ CSharpPartOne/5. ConditionalStatements/06. TheBiggestOfFiveNumbers/TheBiggestOfFiveNumbers.cs
@@ -7,7 +7,43 @@ using System;
     {
         static void Main()
         {
+            Console.Write("Enter the first number: ");
+        double a = double.Parse(Console.ReadLine());
+		Console.WriteLine();
+		
+        Console.Write("Enter the second number: ");
+        double b = double.Parse(Console.ReadLine());
+		Console.WriteLine();
+        Console.Write("Enter the third number: ");
+        double c = double.Parse(Console.ReadLine());
+		Console.WriteLine();
+		Console.Write("Enter the fourth number: ");
+        double d = double.Parse(Console.ReadLine());
+		Console.WriteLine();
+		Console.Write("Enter the fifth number: ");
+        double e = double.Parse(Console.ReadLine());
+		Console.WriteLine();
 
+        if (a > b && a > c && a > d && a > e)
+        {
+            Console.WriteLine("The biggest number is: {0}", a);
+        }
+        else if (b > a && b > c && b > d && b > e)
+        {
+            Console.WriteLine("The biggest number is: {0}", b);
+        }
+		else if (c > a && c > b && c > d && c > e)
+        {
+            Console.WriteLine("The biggest number is: {0}", c);
+        }
+		else if (d > a && d > c && d > b && d > e)
+        {
+            Console.WriteLine("The biggest number is: {0}", d);
+        }
+        else
+        {
+            Console.WriteLine("The biggest number is: {0}", e);
+        }
         }
     }
 
