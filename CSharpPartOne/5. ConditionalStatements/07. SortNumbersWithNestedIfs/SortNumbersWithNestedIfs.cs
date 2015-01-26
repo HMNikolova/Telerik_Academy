1990aca812179d0aa70944499505f908155c2ff5
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 11:09:00 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/07. SortNumbersWithNestedIfs/SortNumbersWithNestedIfs.cs
+++ CSharpPartOne/5. ConditionalStatements/07. SortNumbersWithNestedIfs/SortNumbersWithNestedIfs.cs
@@ -9,7 +9,46 @@ using System;
     {
         static void Main()
         {
-
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            double c = double.Parse(Console.ReadLine());
+    
+            if (a > b && a > c)
+            {
+    			if(b > c)
+    			{
+    				Console.WriteLine("{0} {1} {2}", a, b, c);
+    			}
+    			 else
+                {
+                    Console.WriteLine("{0} {1} {2}", a, c, b);
+                }
+            }
+            else if (b > a && b > c)
+            {
+    			if (a > c)
+                {
+                    Console.WriteLine("{0} {1} {2}", b, a, c);
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1} {2}", b, c, a);
+                }
+            }
+            else
+            {
+                if (a > b)
+                {
+                   Console.WriteLine("{0} {1} {2}", c, a, b); 
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1} {2}", c, b, a);
+                }
+            }
         }
     }
 
