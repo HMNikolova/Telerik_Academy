d07753e81ba36a5c334c783d8ba8945a7a9568be
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 08:58:37 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/01. ExchangeIfGreater/ExchangeIfGreater.cs
+++ CSharpPartOne/5. ConditionalStatements/01. ExchangeIfGreater/ExchangeIfGreater.cs
@@ -7,7 +7,27 @@ using System;
     {
         static void Main()
         {   
+            Console.Write("Enter the first variable: ");
             double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter the second variable: ");
+            double b = double.Parse(Console.ReadLine());
+    		double c;
+    
+            if (a < b)
+            {
+                Console.WriteLine("{0} {1}",a,b);
+            }
+            else if (a > b)
+            {
+                c = a;
+                a = b;
+                b = c;
+                Console.WriteLine("{0} {1}",a,b);
+            }
+            else if(a == b)
+            {
+                Console.WriteLine("Wrong input!");
+            }
         }
     }
 
