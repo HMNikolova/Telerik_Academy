4d224abbc13c872330afd1334238c994cfe7e0b3
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 10:09:37 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/04. MultiplicationSign/MultiplicationSign.cs
+++ CSharpPartOne/5. ConditionalStatements/04. MultiplicationSign/MultiplicationSign.cs
@@ -1,6 +1,6 @@
 ﻿//4. Multiplication Sign
 //Write a program that shows the sign (+, - or 0) of the product of three real numbers, without calculating it.
-Use a sequence of if operators.
+//Use a sequence of if operators.
 
 using System;
 
@@ -8,7 +8,22 @@ using System;
     {
         static void Main()
         {
-
+            Console.WriteLine("Enter 3 numbers: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            if (a == 0 || b == 0 || c == 0)
+            {
+                Console.WriteLine("Result = 0");
+            }
+            else if (a > 0 || b > 0 || c > 0)
+            {
+                Console.WriteLine("Result = +");
+            }
+            else
+            {
+                Console.WriteLine("Result = -");
+            }
         }
     }
 
