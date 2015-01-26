b4742d9d6d0d97432b81932b1d3ec52772f35b51
Hristina Nikolova <HMNikolova@gmail.com>
2015-01-26 11:19:32 +0200

    update

--- CSharpPartOne/5. ConditionalStatements/08. DigitAsWord/DigitAsWord.cs
+++ CSharpPartOne/5. ConditionalStatements/08. DigitAsWord/DigitAsWord.cs
@@ -9,7 +9,34 @@ using System;
     {
         static void Main()
         {
+            Console.Write("Enter a digit[0...9]: ");
+        int digit = int.Parse(Console.ReadLine());
 
+        switch (digit)
+        {
+            case 0: Console.WriteLine("zero");
+                break;
+            case 1: Console.WriteLine("one");
+                break;
+            case 2: Console.WriteLine("two");
+                break;
+            case 3: Console.WriteLine("three");
+                break;
+            case 4: Console.WriteLine("four");
+                break;
+            case 5: Console.WriteLine("five");
+                break;
+            case 6: Console.WriteLine("six");
+                break;
+            case 7: Console.WriteLine("seven");
+                break;
+            case 8: Console.WriteLine("eight");
+                break;
+            case 9: Console.WriteLine("nine");
+                break;
+            default: Console.WriteLine("not a digit");
+                break;
+        }
         }
     }
 
