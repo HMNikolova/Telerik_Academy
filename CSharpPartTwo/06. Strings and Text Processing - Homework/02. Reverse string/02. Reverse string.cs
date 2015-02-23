f2fa986e6736312d32f5899110146f065e64d918
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:34:58 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/02. Reverse string/02. Reverse string.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/02. Reverse string/02. Reverse string.cs
@@ -5,5 +5,15 @@ class ReverseString
     static void Main()
     {
         //•	Write a program that reads a string, reverses it and prints the result at the console.
+         
+        Console.Write("Enter string to reverse: ");
+        string input = Console.ReadLine();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reversed.Append(input[i]);
+        }
+        string result = reversed.ToString();
+        Console.WriteLine(result);
     }
 }
