5ae6bdba2abdd3090e1837961a394f70129f0311
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:46:59 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/12. Parse URL/12. Parse URL.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/12. Parse URL/12. Parse URL.cs
@@ -7,5 +7,18 @@ class ParseURL
         /*•	Write a program that parses an URL address given in the format:
          * [protocol]://[server]/[resource] and extracts from it the [protocol],
          * [server] and [resource] elements.*/
+         string url = "http://telerikacademy.com/Courses/Courses/Details/212";
+
+        int index = 0;
+
+        index = url.IndexOf(':');
+        Console.WriteLine("[protocol] = {0}", url.Substring(0,index));
+        url = url.Remove(0, index + 3);
+
+        index = url.IndexOf('/');
+        Console.WriteLine("[server] = {0}", url.Substring(0,index));
+        url = url.Remove(0, index);
+
+        Console.WriteLine("[resource] = {0}", url);
     }
 }
