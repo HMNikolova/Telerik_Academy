3a7b2dc5ac369f7fca18cf860b30c53af113715b
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:50:57 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/18. Extract e-mails/18. Extract e-mails.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/18. Extract e-mails/18. Extract e-mails.cs
@@ -6,7 +6,18 @@ class ExtractEMails
     {
        //•	Write a program for extracting all email addresses from given text.
        //•	All sub-strings that match the format @… should be recognized as emails.
+        string text =  "Email examples example@abv.bg or dwayne.johnson@gmail.com are valid.These emails: example@yahoo.  @academy.com how@you.is. are not valid.";
+        string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        string regex = @"^([a-zA-Z0-9_\-][a-zA-Z0-9_\-\.]{0,49})" + @"@(([a-zA-Z0-9][a-zA-Z0-9\-]{0,49}\.)+[a-zA-Z]{2,4})$";
 
+        Console.WriteLine("Extracted e-mail addresses: ");
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (Regex.IsMatch(words[i],regex))
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
     }
 }
 
