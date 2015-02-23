aaa1a7ab0cbecc0d0fd38d8b0a3561c1e605f5ef
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:45:01 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/09. Forbidden words/09. Forbidden words.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/09. Forbidden words/09. Forbidden words.cs
@@ -7,7 +7,17 @@ class ForbiddenWords
        /* •	We are given a string containing a list of forbidden words and a text containing some of 
         * these words.
           •	Write a program that replaces the forbidden words with asterisks.*/
+         string text = "Microsoft announced its next generation PHP compiler today. It is based on .NET Framework 4.0 and is implemented as a dynamic language in CLR.";
 
+        string[] words = text.Split('.', ' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i] == "PHP" || words[i] == "CLR" || words[i] == "Microsoft")
+            {
+                text = text.Replace(words[i], new string('*', words[i].Length));
+            }
+        }
+        Console.WriteLine(text);
     }
 }
 
