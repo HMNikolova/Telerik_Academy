cede6fb5b6a889c61ab7748698d4c97b5a0bc688
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:43:17 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/07. Encode-decode/07. Encode-decode.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/07. Encode-decode/07. Encode-decode.cs
@@ -9,6 +9,18 @@ class EncodeDecode
           •	The encoding/decoding is done by performing XOR (exclusive or) operation over the first 
          * letter of the string with the first of the key, the second – with the second, etc. When 
          * the last key character is reached, the next is the first.*/
+        string text = "The key is important.Don't forget your key.";
+        string key = "key";
+        char[] charArr = text.ToCharArray();
 
+        for (int i = 0; i < charArr.Length; i++)
+        {
+            for (int j = 0; j < key.Length; j++)
+            {
+                charArr[i] ^= key[j];
+            }
+        }
+        text = new string(charArr);
+        Console.WriteLine(text);
     }
 }
