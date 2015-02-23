3d5100b3f8d5e7ae3864c4d067ca7c7451e8017b
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:39:56 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/04. Sub-string in text/04. Sub-string in text.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/04. Sub-string in text/04. Sub-string in text.cs
@@ -5,6 +5,19 @@ class SubStringInText
     static void Main()
     {
        //• Write a program that finds how many times a sub-string is contained in a given text (perform case insensitive search).
-    }
+    string text = @"We are living in an yellow submarine. We don't have anything else. 
+            Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.";
+        string sub = "in";
+        int count = 0;
+        for (int i = 0; i < text.Length - 1; i++)
+        {
+            if (text.Substring(i, sub.Length).ToLower() == sub)
+            {
+                count++;
+                i += sub.Length;
+            }
+        }
+        Console.WriteLine(count);
+ }
 }
 
