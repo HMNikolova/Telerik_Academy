66445bc660586c74791973fb5cd2196a44c3039c
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:44:19 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/08. Extract sentences/08. Extract sentences.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/08. Extract sentences/08. Extract sentences.cs
@@ -5,6 +5,18 @@ class ExtractSentences
     static void Main()
     {
         //•	Write a program that extracts from a given text all sentences containing given word.
+        string text = @"We are living in a yellow submarine. We don't have anything else. Inside the submarine is very tight. 
+    So we are drinking all the day. We will move out of it in 5 days.";
+        string[] sentence = text.Split('.');
+
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (sentence[i].Contains(" in ") || sentence[i].Contains("In ") || sentence[i].Contains(" in"))
+            {
+                Console.Write(sentence[i].Trim() + ".");
+            }
+        }
+        Console.WriteLine();
     }
 }
 
