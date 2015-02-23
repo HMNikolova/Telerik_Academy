72f6123a73c3e1941b3d940da8facd1d826291f4
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:52:50 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/20. Palindromes/20. Palindromes.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/20. Palindromes/20. Palindromes.cs
@@ -5,5 +5,39 @@ class Palindromes
     static void Main()
     {
         //•	Write a program that extracts from a given text all palindromes, e.g. ABBA, lamal, exe.
+    
+        string text = "I listen to ABBA.I am gosho i like lamal, I run exe and study in telerik.I work hamal, lol.";
+
+        List<char> punctuation = new List<char>();
+
+        for (int i = 0; i < 128; i++)
+        {
+            if (char.IsPunctuation((char)i))
+            {
+                punctuation.Add((char)i);
+            }
+        }
+        punctuation.Add((char)32);
+
+        string[] words = text.Split(punctuation.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+        Console.WriteLine("Palindromes: ");
+
+        foreach (string word in words)
+        {
+            bool isPalindrome = true;
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome && word.Length > 1)
+            {
+                Console.WriteLine(word);
+            }
+        }
     }
 }
