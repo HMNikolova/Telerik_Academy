819d7ca65c6989b03eb297cbe453cc8d0fc2efdc
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:55:04 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/23. Series of letters/23. Series of letters.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/23. Series of letters/23. Series of letters.cs
@@ -6,6 +6,19 @@ class SeriesOfLetters
     {
         //•	Write a program that reads a string from the console and replaces all series 
         //  of consecutive identical letters with a single one.
+        string text = "aaaaabbbbbcdddeeeedssaa";
+        StringBuilder letters = new StringBuilder();
+
+        letters.Append(text[0]);
+
+        for (int i = 1; i < text.Length; i++)
+        {
+            if (text[i] != text[i - 1])
+            {
+                letters.Append(text[i]);
+            }
+        }
+        Console.WriteLine(letters.ToString());
     }
 }
 
