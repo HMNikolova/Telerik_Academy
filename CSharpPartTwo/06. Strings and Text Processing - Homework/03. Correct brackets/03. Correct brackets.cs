efb3e9a61d7d9e712fe1eddbc7eed280b2b2c83a
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:36:36 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/03. Correct brackets/03. Correct brackets.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/03. Correct brackets/03. Correct brackets.cs
@@ -5,7 +5,34 @@ class CorrectBrackets
     static void Main()
     {
         //•	Write a program to check if in a given expression the brackets are put correctly.
+        static bool CheckExpression(string expression)
+    {
+        const char leftBracket = '(';
+        const char rightBracket = ')';
+        uint bracketCount = 0;
 
+        for (int i = 0; i < expression.Length; i++)
+        {
+            switch (expression[i])
+            {
+                case leftBracket: bracketCount++; continue;
+                case rightBracket: bracketCount--; continue;
+                default: continue;
+            }
+        }
+        if (bracketCount == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    static void Main()
+    {
+        string expression = Console.ReadLine();
+
+        Console.WriteLine(CheckExpression(expression) ? "Valid expression" : "Incorrect expression");
+    }
     }
 }
 
