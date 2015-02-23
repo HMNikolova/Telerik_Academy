b37878857a72b46190a9957771562871aefe0e80
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:42:38 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/06. String length/06. String length.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/06. String length/06. String length.cs
@@ -7,6 +7,15 @@ class StringLength
         /*•	Write a program that reads from the console a string of maximum 20 characters. If the 
          * length of the string is less than 20, the rest of the characters should be filled with *.
           •	Print the result string into the console.*/
+            string text = Console.ReadLine();
 
+        if (text.Length < 20)
+        {
+            Console.WriteLine(text.PadRight(20, '*'));
+        }
+        else
+        {
+            Console.WriteLine(text);
+        }
     }
 }
