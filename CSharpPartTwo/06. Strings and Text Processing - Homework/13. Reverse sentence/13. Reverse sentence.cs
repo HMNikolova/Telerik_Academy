e005f10ebf526f39b378b32204ba67ca6fcf07fe
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:47:41 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/13. Reverse sentence/13. Reverse sentence.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/13. Reverse sentence/13. Reverse sentence.cs
@@ -5,6 +5,41 @@ class ReverseSentence
     static void Main()
     {
         //•	Write a program that reverses the words in given sentence.
+         string text = "C# is not C++, not PHP and not Delphi!";
+
+        string[] words = text.Split(new char[] { ' ', '!', '?', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+        //creating List to exclude the following chars from it.This list is for the spliting elements like ' ','.', etc.
+        List<char> signs = new List<char>();
+
+        for (char i = 'a'; i <= 'z'; i++)
+        {
+            signs.Add(i);
+        }
+        for (char i = 'A'; i <= 'Z'; i++)
+        {
+            signs.Add(i);
+        }
+        for (char i = '0'; i <= '9'; i++)
+        {
+            signs.Add(i);
+        }
+        signs.Add('#');
+        signs.Add('+');
+
+        //this array holds the spliting elements
+        string[] splitElements = text.Split(signs.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+        Array.Reverse(words);
+
+        var reversedText = new StringBuilder();
+
+        for (int i = 0; i < splitElements.Length; i++)
+        {
+            reversedText.Append(words[i]);
+            reversedText.Append(splitElements[i]);
+        }
+        Console.WriteLine(reversedText.ToString());
     }
 }
 
