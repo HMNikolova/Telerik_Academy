eb39399b9a525d0934b91ccedeba66e222f00cc3
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-23 08:56:31 +0200

    update

--- CSharpPartTwo/06. Strings and Text Processing - Homework/25. Extract text from HTML/25. Extract text from HTML.cs
+++ CSharpPartTwo/06. Strings and Text Processing - Homework/25. Extract text from HTML/25. Extract text from HTML.cs
@@ -6,6 +6,25 @@ class ExtractTextFromHTML
     {
         //•	Write a program that extracts from given HTML file its title (if available), 
         //  and its body text without the HTML tags.
+        string text = "<html><head><title>News</title></head><body><p><a href=\"http://academy.telerik.com\">Telerik Academy</a>aims to provide free real-world practical training for young people who want to turn into skilful .NET software engineers.</p></body></html>";
+        
+        MatchCollection tags = Regex.Matches(text, @"((?<=^|>)[^><]+?(?=<|$))");
+        int count = 1;
+
+        foreach (Match tag in tags)
+        {
+            if (count == 1)
+            {
+                Console.WriteLine("Title: {0}", tag);
+                Console.Write("Text: ");
+            }
+            else
+            {
+                Console.Write(tag + " ");
+            }
+            count++;
+        }
+        Console.WriteLine();
     }
 }
 
