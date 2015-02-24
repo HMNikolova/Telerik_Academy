e64b8370184e282d8c724928f21adc1afee22c9e
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-24 10:58:48 +0200

    update

--- CSharpPartTwo/Linq/LINQ/Demo/Program.cs
+++ CSharpPartTwo/Linq/LINQ/Demo/Program.cs
@@ -10,6 +10,25 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            //var students = new List<string>();
+            
+            //students.Add("Pesho");
+            //students.Add("Gosho");
+            //students.Add("Ivan");
+            //търсиме всички студенти, които не съдържат буквата "о"
+            //var result = students.Where(st => !st.Contains('o'));
+            //искам всички студенти, чиято първа буква е различна от "P" и след това ми ги сортирай по дължина
+            //var result = students.Where(st => st[0] !='P').OrderBy(st => st.Length);//st[0] =='P' - така ще изкара всички, които започват с "P"
+            //foreach (var item in result)
+            //{
+                //Console.WriteLine(item);
+            //}
+            
+            
+            
+            
+            
+            
         }
     }
 }
