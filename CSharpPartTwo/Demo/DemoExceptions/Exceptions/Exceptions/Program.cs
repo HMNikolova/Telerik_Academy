7f0f61c85444033a5ed0a733da4d0d81bb829bdc
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-26 22:23:53 +0200

    update

---
+++ CSharpPartTwo/Demo/DemoExceptions/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int result = 10 / a;
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Input is not digit!");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot enter zero!");
+            }
+        }
+    }
+}
