ff1c94b33a5d6d60802870bb8047f8c0eefd496f
Hristina Nikolova <HMNikolova@gmail.com>
2015-02-27 22:24:22 +0200

    update

--- CSharpPartTwo/Demo/DemoExceptions/Exceptions/Exceptions/Program.cs
+++ CSharpPartTwo/Demo/DemoExceptions/Exceptions/Exceptions/Program.cs
@@ -1,29 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Exceptions
+public class Program
 {
-    class Program
+    public static void StringManipulation(string text)
     {
-        static void Main(string[] args)
+        if (string.IsNullOrWhiteSpace(text))//ако текста е Null или само спейсове и табулации, това ще върне true, ако текста има смисъл ще върне false
         {
-            try
-            {
-                int a = int.Parse(Console.ReadLine());
-                int result = 10 / a;
-            }
-            catch (FormatException)
-            {
 
-                Console.WriteLine("Input is not digit!");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("You cannot enter zero!");
-            }
         }
     }
+
+    public static void Divide(int a)
+    {
+        if (a == 10)
+        {
+            //throw new IndexOutOfRangeException();
+            throw new FormatException("Format is not valid!");
+            //по някаква причина, ако а = 10 нека това да е грешка
+            //System.ArgumentException-ако аргумента, който е подаден на някаква функция не е адекватен
+            //throw new ArgumentException("Ten is not allowed!");
+        }
+        int result = 10 / a;
+
+    }
+    public static void ReadNumber()
+    {
+        try
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            Divide(a);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Divide failed!");
+            throw;
+            //throw new IndexOutOfRangeException("Sory!", ex);
+        }
+
+    }
+
+    public static void Method()
+    {
+        Method();
+    }
+
+    public static void Main()
+	{
+		//Method();//System.StackOverflowException
+
+		
+		try 
+		{
+			ReadNumber();
+		}
+		catch (DivideByZeroException ex)
+		{
+			
+			//Environment.Exit(0);//гаси програмата, решение за излизане от рекурсия
+		}
+		catch (ArgumentException ex)
+		{
+			//Console.WriteLine("Error!");
+			
+			//Console.WriteLine("End of input reached!");//файл, който няма достатъчно редове
+		}
+		catch
+		{
+			//Console.WriteLine("Error!");//може да го използваме за всякакви грешки
+		}
+		catch (Exception)
+		{
+			Console.WriteLine("Program failed!");
+			throw;
+			//Console.WriteLine(ex.Message);
+		}
+		finally//независимо дали има някакъв exception, това ще се изпълни
+		{
+			//clear ram
+			Console.WriteLine("Always!");
+		}
+		Console.WriteLine("Drugi neshta");
+		 
+	}
 }
---
+++ CSharpPartTwo/Demo/DemoExceptions/Exceptions/TextFile/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        var reader = new StreamReader(@"..\..\file.txt");
+        var writer = new StreamWriter(@"..\..\output.txt");
+        //var reader = new StreamReader("file.txt", Encoding.UTF8);//ако искаме кирилица
+        //string text = reader.ReadToEnd();//връща ни цялото съдържание на файла
+        //string secondLine = reader.ReadLine();//може да четем ред по ред особено ако е голям файла
+        //Console.WriteLine(text + secondLine);
+        //Console.WriteLine(text);
+        //reader.Close();//затваряме StreamReader
+
+        //по-лесният начин за затваряне на файлове
+        using (reader)
+        {
+            //string text = reader.ReadToEnd();
+            //string secondLine = reader.ReadLine();
+            //Console.WriteLine(text + secondLine);
+
+            //четене ред по ред
+            string currentLine = reader.ReadLine();
+            int count = 1;
+
+            using (writer)
+            {
+                while (currentLine != null)//прехвърляне в друг файл
+                {
+                    writer.WriteLine("{0:D2}. {1}", count, currentLine);
+                    count++;
+                    currentLine = reader.ReadLine();
+                }
+            }
+
+            //while(currentLine != null)//докато currentLine е различно от null
+            //{
+            //Console.WriteLine("{0}. {1}", count, currentLine);
+            //count++;
+            //currentLine = reader.ReadLine();//чети ми нова линия
+            //}
+        }
+
+        //var writer = new StreamWriter("numbers.txt");
+        using (writer)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                writer.WriteLine(i);
+                //writer.Write(i + " ");
+            }
+        }
+    }
+}
