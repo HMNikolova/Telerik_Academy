b01bcdd3aa24ecaa369a17d7fbae038347cdcb9f
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-15 20:20:27 +0200

    update

---
+++ Exam/C#OOPExampleTest/Defining Classes-2/Defining Classes-2/05.GenericList/MainProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.GenericList
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
