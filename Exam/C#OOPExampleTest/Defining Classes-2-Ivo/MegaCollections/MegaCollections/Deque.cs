b8b538d0046bec4289d0cc813cfa3c226496523c
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-17 14:57:34 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/Deque.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/Deque.cs
@@ -10,7 +10,8 @@ namespace MegaCollections
         private int frontIndex;
         private int backIndex;
         
-        //казваме, ако някой ми създаде Deque без нищо
+        //казваме, ако някой ми създаде Deque без нищо, какво се случва от този конструктор 
+        //извиквам другия конструктор, който ни създава InitialSize, който взима първоначалната стойност 16
         public Deque() : this(InitialSize)
         {
             //в момента, в който му създам празен Deque, вътре му се създава масив от 16 елемента
@@ -30,5 +31,58 @@ namespace MegaCollections
             this.frontIndex = this.data.Length / 2 - 1;
             this.backIndex = this.data.Length / 2;
         }
+        //глупаво е да може да set-вам count
+        public int Count 
+         {
+            get 
+            {
+                //ако имаме масив с 8 индекса и четири запълнени, например от 4 индекс нататък
+                //формулата ще ни бъде 8(индекса)-3(празни отпред) - 1 = 4(и получаваме четирите запълнени)
+                return this.backIndex - this.frontIndex - 1;
+            }
+        }
+        
+        //Capacity по същия начин
+        public int Capacity 
+        {
+            get 
+            {
+                return this.data.Length;
+            }
+        }
+        
+        public void AddFront(Т element)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void AddBack(Т element)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public T RemoveFront()
+        {
+             throw new NotImplementedException();
+        }
+        
+        public T RemoveBack()
+        {
+             throw new NotImplementedException();
+        }
+        
+        public T PeekFront()
+        {
+            //понеже вървим от средата назад и ако имаме един елемент(примерно числото 5) в frontIndex, а масива е с 4 елемента
+            // за да ни го покаже трябва да напишем това, което е на долния ред
+             return this.data[frontIndex + 1];
+        }
+        
+        public T PeekBack()
+        {
+            //и ето го пък обратиня вариант, ако сме от средата надясно
+            return this.data[backIndex - 1];
+        }
+         //1:50:12
     }
 }
