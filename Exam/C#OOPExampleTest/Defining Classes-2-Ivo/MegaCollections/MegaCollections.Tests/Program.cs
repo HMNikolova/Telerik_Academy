425b845bb711a3d918e3b8ef19e771ca920a4aec
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-17 14:57:38 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections.Tests/Program.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections.Tests/Program.cs
@@ -6,6 +6,19 @@ namespace MegaCollections.Tests
         {
             //първо си добавяме референция към другия обект, за да мога да си довавям класове вътре
             Deque<int> dequeOfIntegers = new Deque<int>();
+            //искаме да добавяме отпред и отзад
+            dequeOfIntegers.AddFront(5);
+            dequeOfIntegers.AddBack(10);
+            //освен, че я маха стойността ни я връща
+            int frontRemove = dequeOfIntegers.RemoveFront();
+            int backRemove = dequeOfIntegers.RemoveBack();
+            //погледни и ми дай стойността, която е най-отпред на този дек(Deque), обаче не я махай
+            int frontPeek = dequeOfIntegers.PeekFront();
+            int backPeek = dequeOfIntegers.PeekBack();
+            //дай ми колко елемента пазя вътре
+            int elements = dequeOfIntegers.Count;
+            int maxCapacity = dequeOfIntegers.Capacity;
+            
             
         }
     }
