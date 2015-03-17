74af90c2e545cfa8c4b3dbceb91c7507dead8739
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-17 13:47:54 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/Deque.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/Deque.cs
@@ -7,22 +7,28 @@ namespace MegaCollections
         private const int InitialSize = 16;
         //като за начало искам масив, който да ми съдържа данните на този deque
         private T[] data;
-        private int frontIndexer;
-        private int backIndexer;
+        private int frontIndex;
+        private int backIndex;
         
-        public Deque()
+        //казваме, ако някой ми създаде Deque без нищо
+        public Deque() : this(InitialSize)
         {
             //в момента, в който му създам празен Deque, вътре му се създава масив от 16 елемента
-            this.data = new T[InitialSize];
+            //за да не преизползваме код като долния ред ползваме горе в конструктура this
+            //this.data = new T[InitialSize];
         }
         
-        public Deque(int InitialSize)
+        public Deque(uint InitialSize)
         {
+            //най-малката стойност е 2(this.data.Length) и го / 2 - 1
             if (InitialSize < 2)
             {
-                throw new IndexOutOfRangeException()
+                throw new IndexOutOfRangeException("Initial size of Deque must be bigger than 2.");
             }
             this.data = new T[InitialSize];
+            //инициализираме си началото и края на масива
+            this.frontIndex = this.data.Length / 2 - 1;
+            this.backIndex = this.data.Length / 2;
         }
     }
 }
