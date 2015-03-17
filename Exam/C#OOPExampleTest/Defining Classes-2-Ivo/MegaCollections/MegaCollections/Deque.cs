d9f849a6e22511314b5bab91ac775adf092d6ff0
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-17 13:04:42 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/Deque.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/Deque.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MegaCollections
 {
-    class Deque
+    using System;
+    //първо трябва да кажа на класа да бъде шаблонен с <T>
+    public class Deque<T>
     {
+        private const int InitialSize = 16;
+        //като за начало искам масив, който да ми съдържа данните на този deque
+        private T[] data;
+        private int frontIndexer;
+        private int backIndexer;
+        
+        public Deque()
+        {
+            //в момента, в който му създам празен Deque, вътре му се създава масив от 16 елемента
+            this.data = new T[InitialSize];
+        }
+        
+        public Deque(int InitialSize)
+        {
+            if (InitialSize < 2)
+            {
+                throw new IndexOutOfRangeException()
+            }
+            this.data = new T[InitialSize];
+        }
     }
 }
