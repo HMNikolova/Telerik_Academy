2a9afd1a92a438cbdb1f68c0146166b4db7ff1da
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-18 14:39:50 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/TestGenerics/TestGenerics/Animal.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/TestGenerics/TestGenerics/Animal.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TestGenerics
 {
-    class Animal
+    public class Animal
     {
+        public string Name { get; set; }
     }
 }
