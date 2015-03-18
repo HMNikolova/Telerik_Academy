deb510b11ca5d058ec1778021c80e717d8b7fa6a
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-18 15:22:23 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/TestGenerics/TestGenerics/Program.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/TestGenerics/TestGenerics/Program.cs
@@ -21,6 +21,21 @@ namespace TestGenerics
             
             //ако имам може второто ограничението да ни е структура
             //GenericClass<Bunny, int>generic = new GenericClass<Bunny, int>();
+            
+            //извикваме му нов масив
+            int max = Max<int>(new int[] {5,6,7});
+       
+        }
+        //може да имаме и generic методи
+        //може да кажем че търсим мах от Т, като подадем масив от Т и ни върне максималния елемент
+        //т.е. не е нужно класа да ми е Generic
+        //може също да ограничаваме
+        //static void Max<T>(T[] data) where T : struct
+        //компилатора се сеща дори и да не е описано, че горе му подавам масив от Int (при методи става, при листове не)
+        
+        static void Max<T>(T[] data)
+        {
+            return data[0];
         }
     }
 }
