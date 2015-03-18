2fb0618abeb4ecd5a1ad7c5960a716a47d8ba4e6
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-18 14:39:54 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/TestGenerics/TestGenerics/Program.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/TestGenerics/TestGenerics/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TestGenerics
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //няма проблем, защото Bunny е Animal
+            //не може да достъпваме някои методи на Bunny, защото съм го запазила като Animal
+            //ако е запазено като Bunny излиза метода
+            //Bunny someBunny = new Bunny("Pesho");
+            Animal someBunny = new Bunny("Pesho");
+            //Т задължително трябва да ми бъде клас, ако му подам нещо, което е от референтен тип([]), работи
+            //GenericClass<int[], string>generic = new GenericClass<int[], string>();
+            //ако искаме да наследява Animal
+            GenericClass<Bunny, string>generic = new GenericClass<Bunny, string>();
+            //щом съм ограничил моя GenericClass и съм му казала искам Т да ми е Animal, няма проблем да го наяиша и така
+            //GenericClass<Animal, string>generic = new GenericClass<Animal, string>();
+            
+            //ако имаме ограничението да ни е структура, обекта трябва да ни изглежда така
+            //GenericClass<int, string>generic = new GenericClass<int, string>();
+            
+            //ако имам може второто ограничението да ни е структура
+            //GenericClass<Bunny, int>generic = new GenericClass<Bunny, int>();
         }
     }
 }
