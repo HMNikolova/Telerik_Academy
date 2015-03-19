dac81f964632fd8807d61173d1861395ce9ef0e1
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-19 12:49:12 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/AuthorAttribute.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/MegaCollections/MegaCollections/AuthorAttribute.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MegaCollections
 {
-    public class AuthorAttribute
+    using System;
+    //ако имаме два автора в Deque, за да не ни изписва грешка, се изписва долното
+    [AttributeUsange(AttributeTargets.All, AllowMultiple = true)]
+    
+    public class AuthorAttribute : Attribute
     {
+        public AuthorAttribute(string name)
+        {
+            
+        }
     }
 }
