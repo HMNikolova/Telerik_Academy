50b8d84a3b8edd37320270e1a8c95e92623bf55e
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-19 21:30:56 +0200

    update

---
+++ Exam/C#OOPExampleTest/EverthingElse/EverthingElse/Main.cs
@@ -0,0 +1,14 @@
+﻿namespace EverthingElse
+{
+    using System;
+
+    public static class Main
+    {
+        static void Main()
+        {
+            //искаме да направим string  да има допълнителна функционалност
+            var text = "Pesho";
+            text.Count
+        }
+    }
+}
---
+++ Exam/C#OOPExampleTest/EverthingElse/EverthingElse/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace EverthingElse
+{
+    public static class StringExtensions
+    {
+        //всеки един Extension метод трябва да бъде статичен
+        //следващото нещо е да сложим думата this като първи аргумент и след това да  сложим какво искаме да екстендваме
+        //9:30
+        public static int CountWords(this string input)
+        {
+            return 0;
+        }
+        //да покажем на c#, че ние искаме да екстендваме стрингове
+        
+    }
+}
