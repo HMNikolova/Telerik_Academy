03de2e8fc4694424d9bc015cb795f303918bdf18
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-19 15:55:36 +0200

    update

--- Exam/C#OOPExampleTest/Defining Classes-2-Ivo/Profiler/Profiler/SaveAttribute.cs
+++ Exam/C#OOPExampleTest/Defining Classes-2-Ivo/Profiler/Profiler/SaveAttribute.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Profiler
 {
-    class SaveAttribute
+    public class SaveAttribute : Attribute
     {
     }
 }
