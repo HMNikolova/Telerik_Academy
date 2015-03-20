0b775fb8d71526dcc402b6a29cd9d350d7268bb4
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-20 12:00:06 +0200

    update

--- Exam/C#OOPExampleTest/EverthingElse/EverthingElse/Main.cs
+++ Exam/C#OOPExampleTest/EverthingElse/EverthingElse/Main.cs
@@ -2,13 +2,19 @@
 {
     using System;
 
-    public static class Main
+    public static class StartingPoint
     {
         static void Main()
         {
             //искаме да направим string  да има допълнителна функционалност
             var text = "Pesho";
-            text.Count
+            
+            //метода така написан в този клас не е нищо особено, то го разбира статичен метод в статичен клас и може да се извиква така:
+            //ние не искаме така
+            //StringExtensions.CountWords();
+            //а да можем да кажем:
+            10:10
+            text.CountWords();
         }
     }
 }
