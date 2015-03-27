e119b90aa99f8a5385fca75fd7a540a6c9b22485
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-27 14:56:52 +0200

    update

--- Exam/OOPCSharp/OOPCSharpDemo/PetStore/PetStore.cs
+++ Exam/OOPCSharp/OOPCSharpDemo/PetStore/PetStore.cs
@@ -8,12 +8,29 @@ namespace PetStore
 
     public class PetStore
     {
+        static IList<IPet> pets = new IList<IPet>();
+        //List<Dolphin> dolphins = new List<Dolphin>();
+        //List<Cat> cats = new List<Cat>();
+        //List<Parrot> parrots = new List<Parrot>();
         static void Main()
         {
             //правим си обекта и си добавяме using AnimalKindom.Models;
             PetHamster hammy = new PetHamster();
             Console.WriteLine(hammy.ProvokeHappiness());
 
+            pets.Add(new Dolphin());
+            pets.Add(new PetHamster());
+            pets.Add(new Cat());
+            
+            ProntToConsole(pets);
+            //ограничавам го че ще работя само с животни, за да мога да достъпя ProvokeHappiness
+            private static void ProntToConsole<T>(ICollection<T> collection) where T : IPet
+        {
+            foreach (var pet in collection)
+            {
+                Console.WriteLine(pet.ProvokeHappiness());
+            }
+            
             //откъде идва класа
             //var type = typeof(Hamster);
             //Console.WriteLine(type.BaseType);
@@ -22,7 +39,29 @@ namespace PetStore
             //list.Add(new Hamster());
             //list.Add(new Monster());
 
-            //2:40:00
+            //List<int> nums = new List<int> {1,2,3,4,5,6};
+            //ProntToConsole(nums);
+            
+            //string[] otherNums = new string[] {"10", "20", "30"};
+            //ProntToConsole(otherNums);
         }
+        
+         //private static void ProntToConsole<T>(ICollection<T> collection)
+        //{
+            //foreach (var num in collection)
+            //{
+                //Console.WriteLine(num);
+            //}
+        //}
+        
+        ////private static void ProntToConsole(List<int> collection)
+        ////{
+            ////foreach (var num in collection)
+            ////{
+                ////Console.WriteLine(num);
+            ////}
+        ////}
+        
+       
     }
 }
