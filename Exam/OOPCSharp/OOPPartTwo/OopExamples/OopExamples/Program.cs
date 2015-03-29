f0c0e1284d90abae98929675d0a42aecd0e15f0c
Hristina Nikolova <HMNikolova@gmail.com>
2015-03-29 18:19:10 +0300

    update

---
+++ Exam/OOPCSharp/OOPPartTwo/OopExamples/OopExamples/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Виртуален метод е метод, който се намира в някакъв базов клас, 
+//и който има ключовата дума в декларацията си virtual. Тази ключова 
+//дума virtual позволява на този метод от базовия клас не наследниците му 
+//да задраскат кода, който е написан в метода и да го подменят(override) 
+//с нов код и тогава вече метода с дума virtual ако променливата стойността 
+//и е инстанция на наследника тогава метода с дума virtual ще се извиква от 
+//наследника. Важно е методите, които са маркирани с virtual, са методи, 
+//които можем да override-нем и да им сменим поведението. Методите ако не са 
+//виртуални и не са абстрактни не може да им бъде подменяно поведението override.
+namespace OopExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //когато променливата е от тип базов клас аз мога да присвоя на нея като стойност всеки един от наследниците на базовия клас
+            Control control = new Menu();
+            control.Draw();
+        }
+    }
+    //ако искам никой да не може да наследява класа ми го правя: public sealed class Control
+    //абстрактните класове не могат да имат инстанция, а когато даден клас е абстрактен мога да правя абстрактни методи вътре в него, методи без поведение
+    public abstract class Control
+    {
+        //казвайки че е виртуален вече всеки мой наследник може да подмени това поведение
+        public virtual void Draw()
+        {
+            Console.WriteLine("Control.Draw");
+        }
+    }
+
+    public class Button : Control
+    {
+        //public new void Draw()//с new и ако нямаме virtual и override избягваме грешки, но това вече не е полиморфизъм и ще се извиква базовия клас
+        public override void Draw()
+        {
+            Console.WriteLine("Button.Draw");
+            //base.Draw();
+        }
+         
+        public void Click()
+        {
+            Console.WriteLine("Button.Click");
+        }
+    }
+
+    public class Menu : Control
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Menu.Draw");
+        }
+    }
+}
---
+++ Exam/OOPCSharp/OOPPartTwo/OopExamples/OopExamples2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//абстрактните методи са виртуални методи (т.е. методи, на които мога да им подменя поведението), 
+//но са методи, които си нямат имплементация (нямат тяло)
+//Може да направим метод, който да няма поведение и да накарам насила
+//всички мои наследници на моя клас да override-нат този метод искат или не искат
+//ако искат да наследяват моя клас с ключовата дума abstract аз ги задължавам да
+//override-нат да сложат някакво поведение на всеки един от тези методи
+namespace OopExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+           Control control = new Menu();
+            control.Draw();
+        }
+    }
+    //когато имаме австрактен метод и класа ни трябва да е абстрактен
+    //и не мога да си позволя волността наследника да не го override-на
+    public abstract class Control : object
+    {
+        //всеки един от моите наследници ще го има, но не може да му промени 
+        //поведението, понеже не е вируален, наследява го, но в този абстрактен 
+        //клас аз имам комбинация от абстрактен метод, които ме задължава да го
+        //имплементирам и метод, който наследявам и използвам наготово и тогава
+        //съответно ще имам метода Draw и всеки мой наследник ще има съответно 
+        //36:24
+
+        public void Hide()
+        {
+            Console.WriteLine("Control.Hide");
+        }
+
+        public abstract void Draw();
+    }
+
+    public class Button : Control
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Button.Draw");
+           
+        }
+
+        public void Click()
+        {
+            Console.WriteLine("Button.Click");
+        }
+    }
+
+    public class Menu : Control
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Menu.Draw");
+        }
+    }
+}
+
