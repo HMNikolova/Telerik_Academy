836b3be0f43590da4df7b804df935eba9cc7e2dd
Hristina Nikolova <HMNikolova@gmail.com>
2015-06-08 16:14:28 +0300

    update

---
+++ WebDesign/hoework js/object.js
@@ -0,0 +1,12 @@
+//var obj = {};//така описваме обект
+console.log(typeof 5);
+console.log(typeof 'string');
+console.log(typeof true);
+console.log(typeof null);//изкарва object, но типа си е null
+console.log(typeof undefined);
+console.log(typeof []);
+console.log(typeof new Date());
+console.log(typeof NaN);
+console.log(3.145646264564864.toFixed(2));
+console.log(NaN.toFixed(2));
+//25:52
